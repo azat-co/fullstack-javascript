ac9aa94fe2aa9c3b23c681f5231bd99a13a3b196
Azat Mardan <hi@azat.co>
2015-12-01 02:44:21 -0800

    changes :golf:

---
+++ 8-test/mb-server.js
@@ -0,0 +1,70 @@
+var http = require('http')
+// Loads http module
+var util= require('util')
+// Usefull functions
+var querystring = require('querystring')
+// Loads querystring module, we'll need it to serialize and deserialize objects and query strings
+
+var messages=[]
+// This array will hold our messages
+messages.push({
+  'name': 'John',
+  'message': 'hi'
+})
+// Sample message to test list method
+
+exports.server=http.createServer(function (req, res) {
+// Creates server
+  if (req.method == 'POST' && req.url == '/messages/create.json') {
+    // If method is POST and URL is messages/ add message to the array
+    var message=''
+    req.on('data', function(data, msg){
+      console.log(data.toString('utf-8'))
+      message=exports.addMessage(data.toString('utf-8'))
+      // Data is type of Buffer and must be converted to string with encoding UTF-8 first
+      // Adds message to the array
+
+    })
+    req.on('end', function(){
+      console.log('message', util.inspect(message, true, null))
+      console.log('messages:', util.inspect(messages, true, null))
+      // Debugging output into the terminal
+      res.writeHead(200, {'Content-Type': 'text/plain'})
+      // Sets the right header and status code
+      res.end(message)
+      // Out put message, should add object id
+    })
+  } else
+  if (req.method=="GET"&&req.url=='/messages/list.json') {
+  // If method is GET and URL is /messages output list of messages
+    var body=exports.getMessages()
+    // Body will hold our output
+    res.writeHead(200, {
+      'Content-Length': body.length,
+      'Content-Type': 'text/plain'
+    })
+    res.end(body)
+  }
+  else {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    // Sets the right header and status code
+    res.end('Hello World\n')
+  }
+  console.log(req.method)
+
+  // Outputs string with line end symbol
+}).listen(process.env.PORT || 1337)
+// Sets port and IP address of the server
+console.log('Server running at http://127.0.0.1:1337/')
+
+
+exports.getMessages = function() {
+  return JSON.stringify(messages)
+  // Output array of messages as a string/text
+}
+exports.addMessage = function (data){
+  messages.push(querystring.parse(data))
+  // To convert string into JavaScript object we use parse/deserializer
+  return JSON.stringify(querystring.parse(data))
+  // Output new message in JSON as a string
+}
