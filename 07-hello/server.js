3c616111714313d91890b8bcde33adc6d2d67e5e
Azat Mardan <hi@azat.co>
2015-12-07 15:08:06 -0800

    changes :blue_book:

--- 07-hello/server.js
+++ 07-hello/server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 1337
 
 var server = http.createServer(function (req, res) { // Creates server
   res.writeHead(200, {'Content-Type': 'text/plain'})   // Sets the right header and status code
-  res.end('Hello World!\n')  // Outputs string with line end symbol
+  res.end('Hello World\n')  // Outputs string with line end symbol
 })
 server.listen(port, function() {
 	console.log('Server is running at %s:%s ', server.address().address, server.address().port) // Sets port and IP address of the server
