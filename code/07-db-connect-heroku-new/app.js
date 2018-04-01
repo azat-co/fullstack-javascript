0dae13c5e57ac39a73d3e063c14a26cd1dca1041
Azat Mardan <hi@azat.co>
2018-04-01 15:46:27 -0700

    add changes :heavy_exclamation_mark:

---
+++ code/07-db-connect-heroku-new/app.js
@@ -0,0 +1,19 @@
+const util = require('util')
+const url = require('url')
+const client = require ('mongodb').MongoClient
+
+// Script that will work on Heroku with MongoLab
+
+const dbConnUrl = process.env.MONGOLAB_URI ||
+  'mongodb://127.0.0.1:27017/test'
+
+console.log('db server: ', dbConnUrl)
+
+client.connect(dbConnUrl, {}, (error, db) => {
+	console.log('error: ', error)
+	db.listCollections().toArray((err, collections) => {
+    console.log('error: ', error)
+		console.log('collections: ', collections)
+    db.close()
+	})
+})
